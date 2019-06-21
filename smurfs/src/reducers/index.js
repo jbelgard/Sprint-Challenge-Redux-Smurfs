bbd2aeaca20d9951fc2175b7e5b348784104621b
Jason Belgard <belgard.jason@gmail.com>
2019-06-21 11:33:26 -0500

    MVP met

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
+import { FETCHING, SUCCESS, FAILURE, ADDING, DELETING } from '../actions';
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -13,6 +13,31 @@
    error: null
  }
 */
+const initialState = {
+  smurfs       : [],
+  fetching     : false,
+  addingSmurf  : false, 
+  updatingSmurf: false ,
+  deletingSmurf: false ,
+  error        : null ,
+}
+
+export const smurfReducer = ( state = initialState, action) => {
+  switch(action.type){
+     case FETCHING:
+          return {...state,   fetching:true}
+     case ADDING:
+          return {...state,   fetching:true}
+     case DELETING:
+          return {...state,   fetching:true}
+     case SUCCESS:
+          return {...state,   smurfs  :action.payload,  fetching:false}
+     case FAILURE:
+          return {...state,   error   :action.payload,  fetching:false}
+     default:
+          return {...state}   ;
+  }
+}
 
 /*
   You'll only need one smurf reducer for this project.
